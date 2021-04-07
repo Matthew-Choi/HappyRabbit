6fe0abea3d3c8816f5a10aff8ca4899b50e2dcd9
Matthew Choi <mchoi17@calpoly.edu>
2021-04-07 14:18:38 -0700

    Servo test code added

--- ServoController.py
+++ ServoController.py
@@ -1,5 +1,10 @@
 # Servo Controller Code
-
+import time
+from adafruit_servokit import ServoKit
 
 def run_servo():
+    kit = ServoKit(channels=16)
+    kit.servo(4).angle = 180
+    time.sleep(1) 
+    kit.servo(4).angle = 0
     print("Servos Running")
--- main.py
+++ main.py
@@ -3,43 +3,53 @@ import time
 from ButtonController import *
 from LEDController import *
 from ServoController import *
+from adafruit_servokit import ServoKit
+
+#servos
+kit = ServoKit(channels=16)
 
 def main():
-    random.seed(time.time())
-    option = random.randint(1, 3)
+    while 1: 
+        random.seed(time.time())
+        option = random.randint(1, 3)
 
-    # Turn on the corresponding LED
-    print("hello")
-    if option == 1:
-        # Turn on red LED
-        blink_led()
-        print("Red")
-    elif option == 2:
-        # Turn on green LED
-        blink_led()
-        print("Green")
-    elif option == 3:
-        # Turn on blue LED
-        blink_led()
-        print("Blue")
+        # Turn on the corresponding LED
+        print("hello")
+        if option == 1:
+            # Turn on red LED
+            blink_led()
+            print("Red (1)")
+        elif option == 2:
+            # Turn on green LED
+            blink_led()
+            print("Green (2)")
+        elif option == 3:
+            # Turn on blue LED
+            blink_led()
+            print("Blue (3)")
 
-    # need code to timeout after x seconds
-    # can be based off difficulty, or can utilize learning
-    # github examples (may only work on pi but that is ok)
-    # https://stackoverflow.com/questions/1335507/keyboard-input-with-timeout
-    # https://gist.github.com/atupal/5865237
+        # need code to timeout after x seconds
+        # can be based off difficulty, or can utilize learning
+        # github examples (may only work on pi but that is ok)
+        # https://stackoverflow.com/questions/1335507/keyboard-input-with-timeout
+        # https://gist.github.com/atupal/5865237
 
-    get_button()
-    choice = input()
+        get_button()
+        choice = input()
 
-    if int(choice) == option:
-        # activate servo control to release treat/reward
-        run_servo()
-        # send call to AWS to store data
-        print("correct!")
-    else:
-        # Negative Feedback?
-        print("incorrect!")
+        if int(choice) == option:
+            # activate servo control to release treat/reward
+            # run_servo()
+            kit.servo[4].angle = 180
+            time.sleep(0.25)
+            kit.servo[4].angle = 0
+            # send call to AWS to store data
+            print("correct!")
+            print("")
+        else:
+            # Negative Feedback?
+            print("incorrect!")
+            print("")
 
 
 if __name__ == "__main__":
