7e88651e8a0374c7e2cb8b7d3874b4ec01a788c8
Matthew Choi <mchoi17@calpoly.edu>
2021-05-03 17:11:41 -0700

    Adapted LED code

--- main.py
+++ main.py
@@ -9,18 +9,18 @@ import board
 import neopixel
 import time
 
-# Configure the setup
+# Configure the setup for LED
 PIXEL_PIN = board.D18 # pin that is connected to the boar
 ORDER = neopixel.RGB # Pixel color channel Order
 NUM_PIXELS = 60 #number of pixels
-pixels = neopixel.NeoPixel(PIXEL_PIN, 120)
+pixels = neopixel.NeoPixel(PIXEL_PIN, 60, brightness=0.2)
 
-#servos
+# servos
 kit = ServoKit(channels=16)
 
 
 def main():
-    setLeds()
+    turnOffAll()
     while 1: 
         random.seed(time.time())
         option = random.randint(1, 3)
@@ -29,15 +29,15 @@ def main():
         print("hello")
         if option == 1:
             # Turn on red LED
-            blink_led()
+            setRed()
             print("Red (1)")
         elif option == 2:
             # Turn on green LED
-            blink_led()
+            setGreen()
             print("Green (2)")
         elif option == 3:
             # Turn on blue LED
-            blink_led()
+            setBlue()
             print("Blue (3)")
 
         # need code to timeout after x seconds
@@ -63,7 +63,7 @@ def main():
             print("incorrect!")
             print("")
 
-def setLeds():
+def setLedsALL():
     # red
     pixels[0] = (255, 0, 0)
     pixels[1] = (255, 0, 0)
@@ -76,15 +76,6 @@ def setLeds():
     pixels[8] = (255, 0, 0)
     pixels[9] = (255, 0, 0)
 
-    # clear
-    pixels[10] = (0, 0, 0)
-    pixels[11] = (0, 0, 0)
-    pixels[12] = (0, 0, 0)
-    pixels[13] = (0, 0, 0)
-    pixels[14] = (0, 0, 0)
-    pixels[15] = (0, 0, 0)
-    pixels[16] = (0, 0, 0)
-    pixels[17] = (0, 0, 0)
 
     # green
     pixels[18] = (0, 255, 0)
@@ -98,16 +89,6 @@ def setLeds():
     pixels[26] = (0, 255, 0)
     pixels[27] = (0, 255, 0)
 
-    # clear
-    pixels[28] = (0, 0, 0)
-    pixels[29] = (0, 0, 0)
-    pixels[30] = (0, 0, 0)
-    pixels[31] = (0, 0, 0)
-    pixels[32] = (0, 0, 0)
-    pixels[33] = (0, 0, 0)
-    pixels[34] = (0, 0, 0)
-    pixels[35] = (0, 0, 0)
-
     # Blue
     pixels[36] = (0, 0, 255)
     pixels[37] = (0, 0, 255)
@@ -119,6 +100,47 @@ def setLeds():
     pixels[43] = (0, 0, 255)
     pixels[44] = (0, 0, 255)
     pixels[45] = (0, 0, 255)
+
+def turnOffAll():
+    pixels.fill((0, 0, 0))
+
+def setRed():
+    pixels[0] = (255, 0, 0)
+    pixels[1] = (255, 0, 0)
+    pixels[2] = (255, 0, 0)
+    pixels[3] = (255, 0, 0)
+    pixels[4] = (255, 0, 0)
+    pixels[5] = (255, 0, 0)
+    pixels[6] = (255, 0, 0)
+    pixels[7] = (255, 0, 0)
+    pixels[8] = (255, 0, 0)
+    pixels[9] = (255, 0, 0)
+
+def setBlue():
+    # Blue
+    pixels[36] = (0, 0, 255)
+    pixels[37] = (0, 0, 255)
+    pixels[38] = (0, 0, 255)
+    pixels[39] = (0, 0, 255)
+    pixels[40] = (0, 0, 255)
+    pixels[41] = (0, 0, 255)
+    pixels[42] = (0, 0, 255)
+    pixels[43] = (0, 0, 255)
+    pixels[44] = (0, 0, 255)
+    pixels[45] = (0, 0, 255)
+
+def setGreen():
+    # green
+    pixels[18] = (0, 255, 0)
+    pixels[19] = (0, 255, 0)
+    pixels[20] = (0, 255, 0)
+    pixels[21] = (0, 255, 0)
+    pixels[22] = (0, 255, 0)
+    pixels[23] = (0, 255, 0)
+    pixels[24] = (0, 255, 0)
+    pixels[25] = (0, 255, 0)
+    pixels[26] = (0, 255, 0)
+    pixels[27] = (0, 255, 0)
 
 
 if __name__ == "__main__":
